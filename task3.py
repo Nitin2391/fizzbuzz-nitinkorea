376e9054105b32f7db4f2492db7da00aad42c28f
Nitin2391 <nt547345@dal.ca>
2019-05-23 07:36:07 -0700

    task3

---
+++ task3.py
@@ -0,0 +1,25 @@
+def bookreading():
+	file_name1=open('book1.txt','r')
+	file_name2=open('book2.txt','r')
+	file_name3=open('book3.txt','r')
+
+def unique_words(20k):
+	d1 = dict()
+	fout = open(book1, "r")
+		line1=lin1.split()
+		for char in book1:
+			if char not in 20k:
+				print(char)
+			elsif:
+		
+		             fout=open(book2,"r")
+               		     line2=line2.split()
+		         for char in book2:
+			      if char not in 20k
+			            print(char)
+      			     for line in fout:
+		               line = line.split()
+	                	if char in book1:
+					if char not in 20k:
+						print(char)
+unique_words()
